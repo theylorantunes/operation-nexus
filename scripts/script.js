19215a0e0b89d23f51429df10e8a91675e05ec0c
theylorantunes <theylorantunes@gmail.com>
2024-11-24 17:40:26 -0300

    version 1.0

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+function openMenu() {
+    document.getElementById("menu").style.left = "0";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeMenu() {
+    document.getElementById("menu").style.left = "-250px";
+    document.getElementById("main").style.marginLeft = "0";
+}
